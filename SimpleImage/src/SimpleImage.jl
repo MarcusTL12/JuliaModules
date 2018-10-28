343b2dd06160c708d28ba487022867e5006528fa
MarcusTL12 <marcuspaafjellet@gmail.com>
2018-10-28 22:25:43 +0100

    simpleimage

--- SimpleImage/src/SimpleImage.jl
+++ SimpleImage/src/SimpleImage.jl
@@ -3,13 +3,11 @@ __precompile__(true)
 module SimpleImage
 
 function load_png(filepath::String)::Array{UInt32, 2}
-	temp = Int32[0, 0, 0]
+	imgptr = ccall((:loadPng, "simplepng"), Ptr{UInt32}, (Cstring,), filepath)
+	
+	w, h = unsafe_wrap(Array{UInt32, 1}, imgptr, (2,))
 
-	imgptr = ccall((:loadPngInt, "simplepng"), Ptr{UInt32}, (Cstring, Ptr{Int32}, Ptr{Int32}, Ptr{Int32}), filepath, pointer(temp), pointer(temp) + 4, pointer(temp) + 8)
-
-	w, h, l = temp
-
-	img = unsafe_wrap(Array{UInt32, 2}, imgptr, (h, w))
+	img = unsafe_wrap(Array{UInt32, 2}, imgptr + 8, (h, w))
 
 	finalizer(free_img, img)
 
@@ -17,12 +15,12 @@ function load_png(filepath::String)::Array{UInt32, 2}
 end
 
 function free_img(img::Array{UInt32, 2})
-	ccall((:deleteImg, "simplepng"), Cvoid, (Ptr{UInt8},), pointer(img))
+	ccall((:freeImg, "simplepng"), Cvoid, (Ptr{UInt8},), pointer(img) - 8)
 end
 
 function write_png(filepath::String, img::Array{UInt32, 2})
 	h, w = size(img)
-	ccall((:writePngInt, "simplepng"), Cvoid, (Cstring, Ptr{Int32}, Int32, Int32), filepath, pointer(img), w, h)
+	ccall((:writePng, "simplepng"), Cvoid, (Cstring, Ptr{Int32}, Int32, Int32), filepath, pointer(img), w, h)
 end
 
 end
