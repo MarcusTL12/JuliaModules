44e395f9897b3c9b938fa823e5e324c30631d364
MarcusTL12 <marcuspaafjellet@gmail.com>
2018-11-02 16:48:24 +0100

    RGB

--- ImageProcessing/src/ImageProcessing.jl
+++ ImageProcessing/src/ImageProcessing.jl
@@ -1,33 +1,47 @@
 __precompile__(true)
 
-import SimpleImage
-
 
 module ImageProcessing
 
+import SimpleImage
 
-loadImgABGR(filepath::String)::Array{UInt32, 2} = SimpleImage.load_png(filepath)
+ColTup = Tuple{UInt8, UInt8, UInt8, UInt8}
+ImageA = Array{UInt32, 2}
 
-writeImgABGR(filepath::String, img::Array{UInt32, 2}) = SimpleImage.write_png(filepath, img)
+
+loadImgABGR(filepath::String)::ImageA = SimpleImage.load_png(filepath)
+
+writeImgABGR(filepath::String, img::ImageA) = SimpleImage.write_png(filepath, img)
 
 
-getARGB(color::UInt32, abgr::Bool=false)::Tuple{UInt8, UInt8, UInt8, UInt8} = (color >> 24 & 0xff, color >> (abgr ? 0 : 16) & 0xff, color >> 8 & 0xff, color >> (abgr ? 16 : 0) & 0xff)
+getARGB(color::UInt32, abgr::Bool=false)::ColTup = (color >> 24 & 0xff, color >> (abgr ? 0 : 16) & 0xff, color >> 8 & 0xff, color >> (abgr ? 16 : 0) & 0xff)
 
-getColorInt(color::Tuple{UInt8, UInt8, UInt8, UInt8}, abgr::Bool=false)::UInt32 = UInt32(color[1]) << 24 | UInt32(color[2]) << (abgr ? 0 : 16) | UInt32(color[3]) << 8 | UInt32(color[4]) << (abgr ? 16 : 0)
+getColorInt(color::ColTup, abgr::Bool=false)::UInt32 = UInt32(color[1]) << 24 | UInt32(color[2]) << (abgr ? 0 : 16) | UInt32(color[3]) << 8 | UInt32(color[4]) << (abgr ? 16 : 0)
 
 
 convertbetweenABGR(color::UInt32)::UInt32 = getColorInt(getARGB(color, true))
 
-convertbetweenABGR(color::Tuple{UInt8, UInt8, UInt8, UInt8})::Tuple{UInt8, UInt8, UInt8, UInt8} = getARGB(getColorInt(color, true))
+convertbetweenABGR(color::ColTup)::ColTup = getARGB(getColorInt(color, true))
 
 
-function loadImgARGB(filepath::String)::Array{UInt32, 2}
-	img = loadImgABGR(filepath)
-	for i in 1 : len(img)
+function convertbetweenABGR(img::ImageA)
+	for i in 1 : length(img)
 		img[i] = convertbetweenABGR(img[i])
 	end
 end
 
 
+function loadImgARGB(filepath::String)::ImageA
+	img = loadImgABGR(filepath)
+	convertbetweenABGR(img)
+	return img
 end
 
+
+function writeImgARGB(filepath::String, img::ImageA)
+	convertbetweenABGR(img)
+	writeImgABGR(filepath, img)
+	convertbetweenABGR(img)
+end
+
+end
