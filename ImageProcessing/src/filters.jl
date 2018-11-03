b8b555d9d6c339491200c846d2f3e64cd43a0a7a
MarcusTL12 <marcuspaafjellet@gmail.com>
2018-11-04 00:50:35 +0100

    Added propper gaussian blur

--- ImageProcessing/src/filters.jl
+++ ImageProcessing/src/filters.jl
@@ -18,7 +18,7 @@ function grayscale!(img::ImgFlt)
 end
 
 
-function simple_gaussian!(img::ImgFlt)
+function simple_blur!(img::ImgFlt)
 	kernel = Float32[
 		1 2 1;
 		2 3 2;
@@ -26,5 +26,22 @@ function simple_gaussian!(img::ImgFlt)
 	]
 	apply_kernel!(img, kernel)
 	img ./= sum(kernel)
+end
+
+
+function gaussian_kernel(r::Int, stdev::Number)::Array{Float32, 2}
+	r2(x, y) = x^2 + y^2
+	kernel::Array{Float32, 2} = Array{Float32, 2}(undef, (2 * r + 1, 2 * r + 1))
+	for i in CartesianIndices(size(kernel))
+		x, y = Tuple(i)
+		kernel[x, y] = (1 / (sqrt(2 * pi) * stdev)) * exp(- r2(x - r, y - r) / (2 * stdev^2))
+	end
+	kernel ./= sum(kernel)
+	return kernel
+end
+
+
+function gaussian!(img::ImgFlt, r::Int, stdev::Number)
+	apply_kernel!(img, gaussian_kernel(r, stdev))
 end
 
