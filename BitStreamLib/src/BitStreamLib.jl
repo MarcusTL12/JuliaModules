7818d578802500d20705249de8494ea88108215b
MarcusTL12 <marcuspaafjellet@gmail.com>
2018-12-04 19:14:51 +0100

    Updated BitstreamLib with basic array properties

--- BitStreamLib/src/BitStreamLib.jl
+++ BitStreamLib/src/BitStreamLib.jl
@@ -5,17 +5,22 @@ module BitStreamLib
 
 import Base.push!
 import Base.length
-import Base.print
-import Base.println
 import Base.get
+import Base.getindex
+import Base.setindex!
+import Base.string
+import Base.show
 
 export BitStream
 export push!
 export get
+export getindex
 export set
+export setindex!
 export length
-export print
-export println
+export string
+export show
+
 
 
 struct BitStream
@@ -24,6 +29,8 @@ struct BitStream
 	BitStream() = new(zeros(UInt8, 16), [1, 1])
 	BitStream(prealloc::Int) = new(zeros(UInt8, prealloc), [1, 1])
 	BitStream(data::Array{UInt8, 1}, len::Int) = new(data, [fld(len, 8) + 1, (len) % 8 + 1])
+	BitStream(data::Array{UInt8, 1}) = BitStream(data, length(data) * 8)
+	BitStream(data::String) = BitStream([UInt8(i) for i in data])
 end
 
 
@@ -41,9 +48,15 @@ function push!(strm::BitStream, data::Bool)
 end
 
 
+getindex(A::BitStream, i1::Int)::Bool = get(A, i1)
+
+
 function get(strm::BitStream, index::Int)::Bool
 	return (strm.data[cld(index, 8)] & (UInt8(1) << ((index - 1) % 8))) != 0
 end
+
+
+setindex!(A::BitStream, val::Bool, i1::Int) = set(A, i1, val)
 
 
 function set(strm::BitStream, index::Int, val::Bool)
@@ -57,16 +70,18 @@ function length(strm::BitStream)::Int
 end
 
 
-function print(strm::BitStream)
+function string(strm::BitStream)
+	ret = ""
 	for i in 1 : length(strm)
-		print(get(strm, i) ? '1' : 0)
+		ret *= string(strm[i] ? '1' : 0)
 	end
+	return ret
 end
 
 
-function println(strm::BitStream)
-	print(strm)
-	print('\n')
+function show(io::IO, strm::BitStream)
+	print(io, string(strm))
 end
+
 
 end
--- Stego/src/Stego.jl
+++ Stego/src/Stego.jl
@@ -3,14 +3,17 @@ __precompile__(true)
 
 module Stego
 
+using BitStreamLib
 
-function test(s)
-	println(s)
-end
+str_dat(s::String)::Array{UInt8, 1} = unsafe_wrap(Array{UInt8, 1}, pointer(s), length(s))
 
+lsb(img::Array{UInt8, 3}, data::String) = lsb(img, str_dat(data))
 
-function lsb(img::Array{UInt8, 3})
-	map!(col -> 0xff - col, img, img)
+function lsb(img::Array{UInt8, 3}, data::Vector{UInt8})
+	data_bits::BitStream = BitStream(data)
+
+	img .&= 0xfe
+
 end
 
 
