b08a3dbd8ccd83c7bcb7e9065ec24f40ba8e55f4
MarcusTL12 <marcuspaafjellet@gmail.com>
2018-12-06 22:32:15 +0100

    Done for now

--- MathParse/src/MathParse.jl
+++ MathParse/src/MathParse.jl
@@ -4,8 +4,34 @@ __precompile__(true)
 module MathParse
 
 
-function test()
-	println("Helloen")
+function replacewithfunc(s::String, m::Regex, f)
+	while (i = match(m, s)) != nothing
+		s = replace(s, m => f(i.captures), count=1)
+	end
+	return s
+end
+
+
+function mparse(s::String)
+	s = replace(s, r"\s+" => "")
+	s = replace(s, r"(\d)\(" => s"\g<1>*(")
+	s = replace(s, r"\)(\d)" => s")*\g<1>")
+	s = replace(s, r"\)\(" => s")*(")
+	s = replacewithfunc(s, r"\(([^()]*)\)", (t) -> mparse(String(t[1])))
+	@show s
+	
+	r = [(r"(\-?\d+\.?\d*)/(\-?\d+\.?\d*)", /),
+		(r"(\-?\d+\.?\d*)\*(\-?\d+\.?\d*)", *),
+		(r"(\-?\d+\.?\d*)\+(\-?\d+\.?\d*)", +),
+		(r"(\-?\d+\.?\d*)\-(\-?\d+\.?\d*)", -)]
+
+	apply(t, f) = string(f(parse(Float64, t[1]), parse(Float64, t[2])))
+
+	for i in r
+		s = replacewithfunc(s, i[1], (t) -> apply(t, i[2]))
+	end
+
+	return s
 end
 
 
