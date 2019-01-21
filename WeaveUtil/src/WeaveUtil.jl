9462ab735c15cdfdf3ac20c4c02a2c6936d252e7
MarcusTL12 <marcuspaafjellet@gmail.com>
2019-01-21 21:50:54 +0100

    Added recursion and doctype and docname keyword arguments

--- WeaveUtil/src/WeaveUtil.jl
+++ WeaveUtil/src/WeaveUtil.jl
@@ -4,24 +4,32 @@ module WeaveUtil
 
 using Weave
 
-function buildJMD(inputpath::String)
+function getJMD(to::IOStream, from::IOStream)
 	r = r"input\((.+)\)"
+	for i in eachline(from)
+		if (m = match(r, i)) != nothing
+			open(m.captures[1], "r") do subfile
+				getJMD(to, subfile)
+			end
+		else
+			write(to, i, '\n')
+		end
+	end
+end
+
+
+export buildJMD
+function buildJMD(inputpath::String;
+		doctype="pandoc2pdf", docname=split(inputpath, '.')[end - 1] * ".pdf")
 	open(inputpath, "r") do from
 		open("_tmp.jmd", "w") do to
-			for i in eachline(from)
-				if (m = match(r, i)) != nothing
-					open(m.captures[1], "r") do subfile
-						write(to, subfile, '\n')
-					end
-				else
-					write(to, i, '\n')
-				end
-			end
+			getJMD(to, from)
 		end
 	end
 
-	weave("_tmp.jmd", doctype="pandoc2pdf")
-
+	weave("_tmp.jmd"; doctype=doctype)
+	
+	mv("_tmp.pdf", docname; force=true)
 	rm("_tmp.jmd")
 end
 
